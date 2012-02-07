23dd67da90d0a42aeaf70ffd6eb99099b60359e0
Smotko <ensmotko@gmail.com>
2012-02-07 23:25:03 +0100

    Drawing a happy triangle!

--- galaxyCar/src/org/psywerx/car/GalaxyCarActivity.java
+++ galaxyCar/src/org/psywerx/car/GalaxyCarActivity.java
@@ -1,13 +1,88 @@
 package org.psywerx.car;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
 import android.app.Activity;
+import android.graphics.PixelFormat;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
-public class GalaxyCarActivity extends Activity {
+public class GalaxyCarActivity extends Activity implements GLSurfaceView.Renderer {
     /** Called when the activity is first created. */
+	
+	private GLSurfaceView glView;
+	private FloatBuffer triangleVB;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        
+        glView = new GLSurfaceView(this);
+    	glView.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
+		glView.getHolder().setFormat(PixelFormat.TRANSLUCENT);
+    	glView.setRenderer(this);
+        setContentView(glView);
+    }
+
+	@Override
+	public void onDrawFrame(GL10 gl) {
+		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+		gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+		// Draw the triangle
+        gl.glColor4f(0.63671875f, 0.76953125f, 0.22265625f, 0.0f);
+        gl.glVertexPointer(3, GL10.GL_FLOAT, 0, triangleVB);
+        gl.glDrawArrays(GL10.GL_TRIANGLES, 0, 3);
+		
+	}
+
+	@Override
+	public void onSurfaceChanged(GL10 gl, int width, int height) {
+		gl.glViewport(0, 0, width, height);
+		
+	}
+
+	@Override
+	public void onSurfaceCreated(GL10 gl, EGLConfig arg1) {
+        gl.glClearColor(0.1f, 0.5f, 0.5f, 1.0f);
+        initShapes();
+	}
+	
+    @Override
+    protected void onResume(){
+    	super.onResume();
+
+        glView.onResume();
+    }
+    
+    @Override
+    protected void onPause(){
+    	super.onPause();
+
+   		glView.onPause();
+    }
+    
+    private void initShapes(){
+        
+        float triangleCoords[] = {
+            // X, Y, Z
+            -0.5f, -0.25f, 0,
+             0.5f, -0.25f, 0,
+             0.0f,  0.559016994f, 0
+        }; 
+        
+        // initialize vertex Buffer for triangle  
+        ByteBuffer vbb = ByteBuffer.allocateDirect(
+                // (# of coordinate values * 4 bytes per float)
+                triangleCoords.length * 4); 
+        vbb.order(ByteOrder.nativeOrder());// use the device hardware's native byte order
+        triangleVB = vbb.asFloatBuffer();  // create a floating point buffer from the ByteBuffer
+        triangleVB.put(triangleCoords);    // add the coordinates to the FloatBuffer
+        triangleVB.position(0);            // set the buffer to read the first coordinate
+    
     }
 }
