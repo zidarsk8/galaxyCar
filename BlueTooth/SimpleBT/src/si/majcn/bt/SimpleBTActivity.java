228023735626994d80284364dc96b9eab55894aa
Gregor Majcen <majcn.m@gmail.com>
2012-02-09 18:37:20 +0100

    dodan tezenje za BT

--- BlueTooth/SimpleBT/src/si/majcn/bt/SimpleBTActivity.java
+++ BlueTooth/SimpleBT/src/si/majcn/bt/SimpleBTActivity.java
@@ -2,6 +2,7 @@ package si.majcn.bt;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -18,11 +19,36 @@ public class SimpleBTActivity extends Activity {
         
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         
-        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+        if (mBluetoothAdapter == null) {
             Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
             finish();
             return;
         }
+        
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, 1);
+        } else {
+        	Toast.makeText(this, "Bluetooth enabled", Toast.LENGTH_LONG).show();
+        }
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	switch(requestCode) {
+		case 1:
+			if (resultCode == Activity.RESULT_OK) {
+				Toast.makeText(this, "BT enabled", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "BT not enabled", Toast.LENGTH_SHORT).show();
+                Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableIntent, 1);
+            }
+			break;
+		default:
+			break;
+		}
+    	super.onActivityResult(requestCode, resultCode, data);
     }
     
     public void onClickButton(View v)
