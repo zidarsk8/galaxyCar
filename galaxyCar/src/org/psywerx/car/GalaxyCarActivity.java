62e9292b05b111eff0d26a369825dd2aeca2d956
Smotko <ensmotko@gmail.com>
2012-02-09 11:27:39 +0100

    Full screen & wake lock

--- galaxyCar/src/org/psywerx/car/GalaxyCarActivity.java
+++ galaxyCar/src/org/psywerx/car/GalaxyCarActivity.java
@@ -1,41 +1,59 @@
 package org.psywerx.car;
 
 import android.app.Activity;
-import android.content.res.AssetManager;
+import android.content.Context;
 import android.graphics.PixelFormat;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.os.PowerManager;
+import android.os.PowerManager.WakeLock;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class GalaxyCarActivity extends Activity {
-	
+
 	private GLSurfaceView mGlView;
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		Context context = getApplicationContext();
+		
+		// Enable full screen mode:
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+		// Get wake lock:
+		PowerManager pm = (PowerManager) context
+				.getSystemService(context.POWER_SERVICE);
+		WakeLock wl = pm.newWakeLock(pm.FULL_WAKE_LOCK, "Sandboc lock");
+		wl.acquire();
+
 		setContentView(R.layout.main);
 		this.mGlView = (GLSurfaceView) this.findViewById(R.id.glSurface);
-		if (this.mGlView != null){
-    		this.mGlView.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
+		if (this.mGlView != null) {
+			this.mGlView.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
 			this.mGlView.getHolder().setFormat(PixelFormat.TRANSLUCENT);
-			this.mGlView.setRenderer(new CarSurfaceViewRenderer(getResources().getAssets()));
+			this.mGlView.setRenderer(new CarSurfaceViewRenderer(getResources()
+					.getAssets()));
 			this.mGlView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
 		}
 	}
 
-    @Override
-    protected void onResume(){
-    	super.onResume();
+	@Override
+	protected void onResume() {
+		super.onResume();
 
-        mGlView.onResume();
-    }
-    
-    @Override
-    protected void onPause(){
-    	super.onPause();
+		mGlView.onResume();
+	}
 
-   		mGlView.onPause();
-    }
-    
+	@Override
+	protected void onPause() {
+		super.onPause();
+
+		mGlView.onPause();
+	}
+
 }
