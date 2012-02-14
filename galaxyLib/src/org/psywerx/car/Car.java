20874b7fc36146580ce62b6d1a8dd9ce1531062b
Smotko <ensmotko@gmail.com>
2012-02-14 01:52:12 +0100

    Improved turning a bit

--- galaxyLib/src/org/psywerx/car/Car.java
+++ galaxyLib/src/org/psywerx/car/Car.java
@@ -1,5 +1,7 @@
 package org.psywerx.car;
 
+import java.util.Random;
+
 import javax.microedition.khronos.opengles.GL10;
 
 import org.psywerx.car.bluetooth.BtHelper;
@@ -14,23 +16,35 @@ public class Car{
 	float angle = 0;
 	private Model car;
 	protected float x = 0f,y = 0f,z = -10f;
-	protected float yaw = 260, pitch = 0, skew = 0;
+	protected float yaw = 180, pitch = 0, skew = 0;
+	private float alpha = 0;
+	private Random random;
 	
 	public void update(){
 		float[] m = b.getLastData();
-		m[5] = 5;
+		m[5] =10;
+		m[4] = 5;
 		
 		// TODO: This sort of works for wheel turn 5, make it work better and for more values
 		
-		yaw += m[5]*0.0285;
+		//yaw += m[5]*0.0285;
 		skew += Math.log(m[5]);
 		if(skew > 1) skew = 1;
 		else if(skew < -1) skew = -1;
 		
-		z += Math.sin(angle)*m[5]*0.008;
-		x += Math.cos(angle)*m[5]*0.008;
+		double tmpZ = Math.sin(5)*m[5];
+		double tmpX = Math.cos(5)*m[5];
 		
-		angle += m[5]*0.0005;
+		x = (float) (tmpZ * Math.cos(alpha) - tmpX * Math.sin(alpha));
+		z = (float) (tmpX * Math.cos(alpha) + tmpZ * Math.sin(alpha));
+		
+		float speed = (float) Math.pow(m[5],-3)*10;
+		
+		alpha += speed;
+		
+		yaw += 57*speed;
+		
+		//angle += m[5]*0.0005;
 		//pitch += 6f;
 		//skew += 5f;
 	}
@@ -49,6 +63,7 @@ public class Car{
 		b = md;
 		car = models.GetModel("car");
 		camera = c;
+		random = new Random();
 	}
 
 
