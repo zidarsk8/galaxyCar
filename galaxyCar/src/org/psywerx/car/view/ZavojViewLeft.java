539cb28d89456bb6b391472d743a82ddc5e2bba5
Gregor Majcen <majcn.m@gmail.com>
2012-02-23 23:24:29 +0100

    dodan zavijanje levo in desno. TODO: postInvalidate() in setText ko je kotLevo % 360 == 0

---
+++ galaxyCar/src/org/psywerx/car/view/ZavojViewLeft.java
@@ -0,0 +1,28 @@
+package org.psywerx.car.view;
+
+import org.psywerx.car.Car;
+import org.psywerx.car.R;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class ZavojViewLeft extends View{
+	private Bitmap mImage = null;
+	
+	public ZavojViewLeft(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		mImage = BitmapFactory.decodeResource(getResources(), R.drawable.turn_meter_palcka);
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		canvas.save();
+		canvas.rotate(Car.turnLeft , mImage.getWidth()/2, mImage.getHeight()/2);
+		canvas.drawBitmap(mImage, 0, 0, null);
+		canvas.restore();
+	}
+}
